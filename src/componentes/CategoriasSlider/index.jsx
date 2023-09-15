5914f13a7c5e17a42f5ba0722c95d8a2cf478012
marcelooliveiramendes <marcelooliveiraah09@gmail.com>
2023-09-15 11:27:38 -0300

    Adicionado novas funcionlidades

--- src/componentes/CategoriasSlider/index.jsx
+++ src/componentes/CategoriasSlider/index.jsx
@@ -1,131 +1,36 @@
-import React from 'react';
+import React, {useState, useEffect, useCallback} from 'react';
 import './CategoriasSlider.scss'
 import { Link } from 'react-router-dom';
-// import { Container } from './styles';
-
+import axios from 'axios';
 
 function CategoriasSlider() {
-  return(
-    <div id="categoriasContainer">
-        <Link to="/menu-escolha/teste">
-            <div className='card'>
-                <img src={'./../../icons/solicEPI.svg'} alt="" srcset="" />
-                teste
-            </div>
-        </Link>
-        <Link to="/menu-escolha/teste">
-            <div className='card'>
-                teste
-            </div>
-        </Link>
-        <Link to="/menu-escolha/teste">
-            <div className='card'>
-                teste
-            </div>
-        </Link>
-        <Link to="/menu-escolha/teste">
-            <div className='card'>
-                teste
-            </div>
-        </Link>
-        <Link to="/menu-escolha/teste">
-            <div className='card'>
-                teste
-            </div>
-        </Link>
-        <Link to="/menu-escolha/teste">
-            <div className='card'>
-                teste
-            </div>
-        </Link>
-        <Link to="/menu-escolha/teste">
-            <div className='card'>
-                teste
-            </div>
-        </Link>
-        <Link to="/menu-escolha/teste">
-            <div className='card'>
-                teste
-            </div>
-        </Link>
-        <Link to="/menu-escolha/teste">
-            <div className='card'>
-                teste
-            </div>
-        </Link>
-        <Link to="/menu-escolha/teste">
-            <div className='card'>
-                teste
-            </div>
-        </Link>
-        <Link to="/menu-escolha/teste">
-            <div className='card'>
-                teste
-            </div>
-        </Link>
-        <Link to="/menu-escolha/teste">
-            <div className='card'>
-                teste
-            </div>
-        </Link>
-        <Link to="/menu-escolha/teste">
-            <div className='card'>
-                teste
-            </div>
-        </Link>
-        <Link to="/menu-escolha/teste">
-            <div className='card'>
-                teste
-            </div>
-        </Link>
-        <Link to="/menu-escolha/teste">
-            <div className='card'>
-                teste
-            </div>
-        </Link>
-        <Link to="/menu-escolha/teste">
-            <div className='card'>
-                teste
-            </div>
-        </Link>
-        <Link to="/menu-escolha/teste">
-            <div className='card'>
-                teste
-            </div>
-        </Link>
-        <Link to="/menu-escolha/teste">
-            <div className='card'>
-                teste
-            </div>
-        </Link>
-        <Link to="/menu-escolha/teste">
-            <div className='card'>
-                teste
-            </div>
-        </Link>
-        <Link to="/menu-escolha/teste">
-            <div className='card'>
-                teste
-            </div>
-        </Link>
-        <Link to="/menu-escolha/teste">
-            <div className='card'>
-                teste
-            </div>
-        </Link>
-        <Link to="/menu-escolha/teste">
-            <div className='card'>
-                teste
-            </div>
-        </Link>
-        <Link to="/menu-escolha/teste">
-            <div className='card'>
-                teste
-            </div>
-        </Link>
-        
-    </div>
-  );
+    const [data, setData] = useState()
+
+    const getData = useCallback(async () => {
+        await axios.get("../data/categorias.json")
+            .then((res) => {return setData(res.data.data)})
+            .catch((err) => {return console.log("Falha ao buscar informações");})
+    }, [])
+    
+    useEffect(() => {
+      getData()
+    }, [getData])
+
+    return(
+        <div id="categoriasContainer">
+            {data &&(
+                data.map((item, index) => (
+                    <Link to={`/menu-escolha/${item.code}`} key={index}>
+                        <div className='card'>
+                            <img src={'./../../icons/solicEPI.svg'} alt=""  />
+                            <p>{item.name}</p>
+                        </div>
+                    </Link>
+                ))
+            )}
+        </div>
+    );
 }
+
 
 export default CategoriasSlider;
--- src/componentes/ProdutosSlider/index.jsx
+++ src/componentes/ProdutosSlider/index.jsx
@@ -1,97 +1,42 @@
-import React from 'react';
+import React, {useState, useEffect, useCallback} from 'react';
 import './ProdutosSlider.scss'
-import { Link } from 'react-router-dom';
-// import { Container } from './styles';
+import { Link, useParams } from 'react-router-dom';
+import axios from 'axios';
 
 function ProdutosSlider() {
+    const [data, setData] = useState()
+    const {categoria} = useParams()
+
+    console.log(categoria);
+
+    const getData = useCallback(async () => {
+        let dataDetails = await axios.get("../data/categorias.json")
+            .then((res) => {return res.data.data.filter(item => item.code === categoria)})
+            .catch((err) => {return console.log("Falha ao buscar informações");})
+
+        setData(dataDetails[0].products)
+        console.log(dataDetails[0]);
+
+    }, [categoria])
+    
+    useEffect(() => {
+      getData()
+    }, [getData])
+
   return(
     <div id="produtosContainer">
-        <Link to='/menu-escolha/teste'>
-            <div className='card'>
-                teste
-            </div>
-        </Link>
-        <Link to='/menu-escolha/teste'>
-            <div className='card'>
-                teste
-            </div>
-        </Link>
-        <Link to='/menu-escolha/teste'>
-            <div className='card'>
-                teste
-            </div>
-        </Link>
-        <Link to='/menu-escolha/teste'>
-            <div className='card'>
-                teste
-            </div>
-        </Link>
-        <Link to='/menu-escolha/teste'>
-            <div className='card'>
-                teste
-            </div>
-        </Link>
-        <Link to='/menu-escolha/teste'>
-            <div className='card'>
-                teste
-            </div>
-        </Link>
-        <Link to='/menu-escolha/teste'>
-            <div className='card'>
-                teste
-            </div>
-        </Link>
-        <Link to='/menu-escolha/teste'>
-            <div className='card'>
-                teste
-            </div>
-        </Link>
-        <Link to='/menu-escolha/teste'>
-            <div className='card'>
-                teste
-            </div>
-        </Link>
-        <Link to='/menu-escolha/teste'>
-            <div className='card'>
-                teste
-            </div>
-        </Link>
-        <Link to='/menu-escolha/teste'>
-            <div className='card'>
-                teste
-            </div>
-        </Link>
-        <Link to='/menu-escolha/teste'>
-            <div className='card'>
-                teste
-            </div>
-        </Link>
-        <Link to='/menu-escolha/teste'>
-            <div className='card'>
-                teste
-            </div>
-        </Link>
-        <Link to='/menu-escolha/teste'>
-            <div className='card'>
-                teste
-            </div>
-        </Link>
-        <Link to='/menu-escolha/teste'>
-            <div className='card'>
-                teste
-            </div>
-        </Link>
-        <Link to='/menu-escolha/teste'>
-            <div className='card'>
-                teste
-            </div>
-        </Link>
-        <Link to='/menu-escolha/teste'>
-            <div className='card'>
-                teste
-            </div>
-        </Link>
+        {data !== undefined &&(
+            data.map((item, index) => (
+                <Link to={`/menu-escolha/${item.code}`} key={index}>
+                    <div className='card'>
+                        <img src={'./../../icons/solicEPI.svg'} alt=""  />
+                        <p>{item.name}</p>
+                    </div>
+                </Link>
+            ))
+        )}
 
+       
         
     </div>
   );
